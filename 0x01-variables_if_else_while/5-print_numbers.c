f3d3b674de247f28b146ed993e8cc9666e831237
mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2022-12-11 10:47:48 -0800

    file: 5-print_numbers commit: fixed some betty styling errors

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int ten;
-	
+
 	for (ten = 0; ten < 10 ; ten++)
 {
 		printf("%d", ten);
