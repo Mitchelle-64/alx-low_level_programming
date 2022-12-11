cdab63d68779f8f540950dd4569b639f5857db6e
mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2022-12-11 10:44:54 -0800

    file: 5-print_numbers commit: used int instead of char

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ten;
+	
+	for (ten = 0; ten < 10 ; ten++)
+{
+		printf("%d", ten);
+}
+	printf("\n");
+	return (0);
+}
