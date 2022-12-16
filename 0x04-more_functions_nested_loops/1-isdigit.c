200393e53064969ac087dd5865c613f086959ce7
mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2022-12-15 16:53:52 -0800

    created the filw 1-isdigit to check if an inputted digit is between 0 and 9.

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - checks if the passed digit it between 0 and 9 .
+ * @c: input digit.
+ * Return: 1 if c is a digit otherwise, returns 0 .
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <=  9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
