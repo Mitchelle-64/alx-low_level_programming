8bdd977d402b95e5bf990358f81733bd4ed197f8
mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2022-12-15 18:28:10 -0800

    created the file 5-more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,24 @@
 #include "main.h"
 /**
- *
+ * more_numbers - a function that rpitns 10 times the numbers, from 0 to 14
+ * 
+ * Return: 0-14 x10 followed by new line
+ */
+
+void more_numbers(void)
+{
+	int i, tn;
+
+	for (tn = '0' ; tn < 10 ; tn++)
+	{
+		for (i = '0' ; i <= 14 ; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i % 10);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
