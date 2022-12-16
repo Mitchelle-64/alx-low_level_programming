40825b2d876fc51ccf372be4197f01c1ed5bb212
mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2022-12-15 17:57:15 -0800

    fixes some betty errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * print_most_numbers - prints number 0 through 9 but excledes 2 and 4
+ *
+ * Return: 0 to 9, excluding 2 and 4 then a new line.
  */
 
 void print_most_numbers(void)
