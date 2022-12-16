b6af4af05422b8d86eefbaaad182161c8f8ebca4
mitchelle-64 <51542475+Mitchelle-64@users.noreply.github.com>
2022-12-15 17:20:51 -0800

    created the file 2-mul.c to multiply 2 digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits
+ * @a: first digit we multiply
+ * @b: second digit to multiply
+ * Return: Product of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
